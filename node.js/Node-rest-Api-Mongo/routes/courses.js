22fd786b3dcead687682ea28d3aafcb19c00044f
sfjbssept <sfjbssept@gmail.com>
2022-09-23 17:55:04 +0100

    Mongo-rest-api-Node

---
+++ node.js/Node-rest-Api-Mongo/routes/courses.js
@@ -0,0 +1,26 @@
+const ex = require("express");
+const Course = require("../models/courses");
+const router = ex.Router();
+
+// get all courses
+router.get("/allcourses", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.json(courses);
+  } catch(err) {
+    res.json(err);
+  }
+});
+
+// add a course
+
+ router.post("/addcourse",async(req,res) => {
+    try{
+      const course= await Course.create(req.body);
+      res.json(course);
+    } catch (err) {
+        res.json(err)
+    }
+ })
+
+module.exports = router;
